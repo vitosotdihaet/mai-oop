e36acc75c3d3fc3aba0ab41a2ba6fb39d38f4946
vitosotdihaet <vitostikitos@gmail.com>
2023-09-18 19:59:23 +0300

    Initial commit

---
+++ l1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+
+
+bool quads(int a, int b) {
+    return std::sqrt(a + b) == trunc(std::sqrt(a + b)) && std::sqrt(a - b) == trunc(std::sqrt(a - b));
+}
+
+std::pair<int, int> closest_pair_tonum(int upper_limit) {
+    for (int i = upper_limit - 1; i > 0; --i) {
+        for (int j = i - 1; j > 0; --j) {
+            if (quads(i, j)) {
+                return std::pair<int, int>(i, j);
+            }
+        }
+    }
+
+    return std::pair<int, int>(0, 0);
+}
+
+
+int main() {
+    int limit = 0;
+    std::cin >> limit;
+
+    std::pair<int, int> p = closest_pair_tonum(limit);
+    std::cout << p.first << '\n';
+    std::cout << p.second << '\n';
+
+    return 0;
+}
