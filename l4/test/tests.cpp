550c1f510b69b7e1d96c39a183d89ffed90776f4
vitosotdihaet <vitostikitos@gmail.com>
2023-11-15 02:56:34 +0300

    Update lab 4 tests

--- l4/test/tests.cpp
+++ l4/test/tests.cpp
@@ -14,7 +14,20 @@ TEST(Compare, All) {
     Rectangle<int> r2(r1);
 
     ASSERT_TRUE(r1 == r2);
+}
+
+TEST(Square, All) {
+    std::vector<Point<float>> points_1 = { Point<float>(0., 0.), Point<float>(0., 1.), Point<float>(1., 1.), Point<float>(0., 1.) };
+    Rectangle<float> r1(points_1);
     ASSERT_TRUE((double) r1 == 1);
+
+    std::vector<Point<float>> points_2 = { Point<float>(0., 0.), Point<float>(3., 0.), Point<float>(2., 1.), Point<float>(1., 1.) };
+    Trapezium<float> t1(points_2);
+    ASSERT_TRUE((double) t1 == 2);
+
+    std::vector<Point<float>> points_3 = { Point<float>(0., 0.), Point<float>(1., 1.), Point<float>(0., 2.), Point<float>(-1., 1.) };
+    Trapezium<float> rh1(points_3);
+    ASSERT_TRUE((double) rh1 == 2);
 }
 
 
